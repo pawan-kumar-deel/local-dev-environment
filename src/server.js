385d1653c7a09b8fc1b44f1045446b01f296c4cc
Pawan Kumar <pawan.kumar@deel.com>
2025-05-28 11:54:33 +0530

    add setup command

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ async function startServer() {
   try {
     const server = app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
+      console.log(`Access the web interface at http://localhost:${PORT}`);
       console.log(`Access the API documentation at http://localhost:${PORT}/api`);
     });
 
